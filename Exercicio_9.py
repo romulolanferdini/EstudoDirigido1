cdbd5110ab5b668531342e1dd0c545e63773dcba
romulolanferdini <49734990+romulolanferdini@users.noreply.github.com>
2019-04-19 20:30:12 -0300

    Exercício_9

---
+++ Exercicio_9.py
@@ -0,0 +1,10 @@
+cigarros = int(input("Quants cigarros você fuma por dia: "))
+anos_fumando = int(input("Anos fumando: "))
+
+total_cigarros = (anos_fumando * 365)*cigarros
+dias_perdidos = (total_cigarros * 10)/24
+
+print ("Dias perdidos %d" %dias_perdidos )
+
+
+
