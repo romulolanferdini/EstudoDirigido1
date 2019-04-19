e364361d87bb2d3e894482e17f85f7837c91a5d5
romulolanferdini <49734990+romulolanferdini@users.noreply.github.com>
2019-04-19 20:08:12 -0300

    Exercício_6

---
+++ Exercicio_6.py
@@ -0,0 +1,6 @@
+distancia = int (input ("Qual a distância a percorrer? "))
+
+velocidade = int (input ("A que velocidade você viajará? "))
+
+print ("Seu tempo de viagem será de", distancia / velocidade,"h")
+
